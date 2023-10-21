90cdb3e0fd57cf748f6818df232a1307077d49b1
Devmustroc <elmustapha.abourar@outlook.com>
2023-10-21 17:27:32 +0200

    :art: NavBar On SideBar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import Link from 'next/link';
 import NextImage from 'next/image';
 import { Box,
     List,
@@ -17,12 +17,50 @@ import {MdHome,
 } from 'react-icons/md'
 
 
+const navMenu =[
+    {
+        name: 'Home',
+        icon: MdHome,
+        route: '/'
+    },
+    {
+        name: 'Search',
+        icon: MdSearch,
+        route: '/search'
+    },
+    {
+        name: 'Your Library',
+        icon: MdLibraryMusic,
+        route: '/library'
+    }
+]
+
 const Sidebar = () => {
     return (
         <Box width="100%" height="calc(100vh - 100px)" bg='black' paddingX="5px" color="gray">
             <Box paddingY="20px">
                 <Box width="120px" marginBottom="20px" paddingX="20px">
                     <NextImage src="/logo.svg" width={120} height={60} />
+                </Box>
+                <Box marginBottom="20px">
+                    <List spacing={2}>
+                        {
+                            navMenu.map((item, index) => {
+                                return (
+                                    <ListItem paddingX='20px' fontSize="16px" key={item.name}>
+                                        <LinkBox>
+                                            <Link href={item.route} passHref>
+                                                <LinkOverlay>
+                                                    <ListIcon as={item.icon} color="white" marginRight="20px" />
+                                                    {item.name}
+                                                </LinkOverlay>
+                                            </Link>
+                                        </LinkBox>
+                                    </ListItem>
+                                )
+                            })
+                        }
+                    </List>
                 </Box>
             </Box>
         </Box>
